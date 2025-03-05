2caa46df79c341f7d3c6971aa5168d03160b925f
ogami-ele <ogami-ele@gmx.com>
2025-03-05 20:18:22 +0000

    Add 1.php

---
+++ src/1.php
@@ -0,0 +1,14 @@
+<?php
+
+// Simple Learning Project
+
+$name = "John";
+$age = 30;
+
+if ($age >= 18) {
+    echo "Hello $name! You are old enough to vote.";
+} else {
+    echo "Sorry, $name. You are not old enough to vote yet.";
+}
+
+?>
