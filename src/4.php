07010d7afbe8422b1d2886e4c39645179cd4f082
ogami-ele <ogami-ele@gmx.com>
2025-03-08 08:34:09 +0000

    Add 4.php

---
+++ src/4.php
@@ -0,0 +1,13 @@
+ <?php
+// Define an array with some animals
+$animals = ['Lion', 'Tiger', 'Elephant'];
+
+// Define an array with some colors
+$colors = ['Red', 'Green', 'Blue'];
+
+// Create a new array that combines the two arrays
+$animalsAndColors = array_merge($animals, $colors);
+
+// Print the combined array
+print_r($animalsAndColors);
+?>
